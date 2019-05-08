81b9583125aa3cd0b3d2aeefcb6e5271610a57bf
tahmislam <37982242+tahmislam@users.noreply.github.com>
2019-05-08 17:01:24 +0600

    Add Users class and updateUserList event

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ io.on('connection', (socket) => {
 		users.removeUser(socket.id);
 		users.addUser(socket.id, params.name, params.room);
 
-		io.to(params.room).emit('UpdateUserList', users.getUserList(params.room));
+		io.to(params.room).emit('updateUserList', users.getUserList(params.room));
 		socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app')); 
 	    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
 		callback();
